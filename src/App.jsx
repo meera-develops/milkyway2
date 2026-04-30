dfab67e05f6121979c60968b61cb20878ac19f5f
Meera Bhola <meerabhola3@gmail.com>
2026-04-29 21:57:27 -0400

    updated viewport across website

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -11,10 +12,17 @@ import { BagProvider } from './components/BagContext';
 import BagDrawer from './components/BagDrawer';
 import './App.css';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
+
 function App() {
 
   return (
     <Router>
+      <ScrollToTop />
       <BagProvider>
         <Navbar />
         <BagDrawer />
